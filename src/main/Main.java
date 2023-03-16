def3322be7b8eb607a5d6749c93a48551e61429e
starrydash <ryclowe@gmail.com>
2023-03-15 20:09:56 -0700

    Restructured program

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
-import item;
+import java.util.Scanner;
+import Item;
+
 public class Main {
     public static void main (String[]args){
         ArrayList<Item> itemList = new ArrayList<Item>();
