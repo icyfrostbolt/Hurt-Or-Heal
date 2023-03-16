a492ba2aecb6ee70fc29c43293c740a54d5da6f6
starrydash <ryclowe@gmail.com>
2023-03-15 19:53:22 -0700

    Initial Commit

---
+++ src/main/java/Item.java
@@ -0,0 +1,25 @@
+public class Item {
+    private name;
+    private int health = 20;
+    private boolean alive = true;
+    public Item(String itemName){
+        name = itemName;
+    }
+    public void hurt(){
+        health -= 2;
+        alive = alive_check();
+    }
+
+    public void heal(){
+        health += 1;
+        alive = alive_check();
+    }
+
+    public boolean alive_check(){
+        if health <= 0;
+            System.out.println(name + " is now at " + health + " health. " + name + " has beem eliminated.");
+            return false;
+        System.out.println(name + " is now at " + health + " health.");
+        return true;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import item;
+public class Main {
+    public static void main (String[]args){
+        ArrayList<Item> itemList = new ArrayList<Item>();
+    }
+}
