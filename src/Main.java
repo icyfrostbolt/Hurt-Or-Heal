1b9f61691356c00d4d5dbdbae169e1bfafb147ad
starrydash <ryclowe@gmail.com>
2023-03-22 15:06:12 -0700

    Final placements announced

--- src/Main.java
+++ src/Main.java
@@ -17,29 +17,37 @@ public class Main {
             }
         }
         while (true) {
-            for (int i = 0; i <= 2; i++){
-                Scanner hurt = new Scanner(System.in);
-                String hurtStringNames = names.toString().substring(1,names.toString().length()-1);
-                System.out.println(hurtStringNames + " are still alive");
-                System.out.print("Hurt: ");
-                String hurtName = hurt.nextLine();
-                Item hurt_item = itemList.get(hurtName);
-                hurt_item.hurt();
-                if (!hurt_item.aliveCheck()){
-                    itemList.remove(hurt_item);
-                    names.remove(hurtName);
-                    finalNames.add(0, hurtName);
-                    int size = names.size()+1;
-                    System.out.println("Their placement was " + size);
+            Scanner hurt = new Scanner(System.in);
+            String hurtStringNames = names.toString().substring(1,names.toString().length()-1);
+            System.out.println(hurtStringNames + " are still alive");
+            System.out.print("Hurt: ");
+            String hurtName = hurt.nextLine();
+            Item hurt_item = itemList.get(hurtName);
+            hurt_item.hurt();
+            if (!hurt_item.aliveCheck()){
+                itemList.remove(hurt_item);
+                names.remove(hurtName);
+                finalNames.add(0, hurtName);
+                int size = names.size()+1;
+                System.out.println("Their placement was " + size + ".");
+                if (names.size() == 1){
+                    finalNames.add(0, names.get(0));
+                    break;
                 }
-                Scanner heal = new Scanner(System.in);
-                String healStringNames = names.toString().substring(1,names.toString().length()-1);
-                System.out.println(healStringNames + " are still alive");
-                System.out.print("Heal: ");
-                String healName = heal.nextLine();
-                Item heal_item = itemList.get(healName);
-                heal_item.heal();
             }
+            Scanner heal = new Scanner(System.in);
+            String healStringNames = names.toString().substring(1,names.toString().length()-1);
+            System.out.println(healStringNames + " are still alive");
+            System.out.print("Heal: ");
+            String healName = heal.nextLine();
+            Item heal_item = itemList.get(healName);
+            heal_item.heal();
+        }
+        int count = 1;
+        System.out.println("\nFinal placements:");
+        for (String finalName : finalNames){
+            System.out.println(count + ". " + finalName);
+            count++;
         }
     }
 }
