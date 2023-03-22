d2f88ea2a4e69f45694a29bf78bce7d5c0817dee
starrydash <ryclowe@gmail.com>
2023-03-21 20:59:24 -0700

    Placement Announcement

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Dictionary<String, Item> itemList = new Hashtable<>();
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> finalNames = new ArrayList<>();
         while (true) {
             Scanner input = new Scanner(System.in);
             System.out.println("Enter name of next participant: ");
@@ -10,21 +12,29 @@ public class Main {
                 break;
             } else {
                 Item itemToBeAdded = new Item(name);
+                names.add(name);
                 itemList.put(name, itemToBeAdded);
             }
         }
         while (true) {
-            Scanner input = new Scanner(System.in);
             for (int i = 0; i <= 2; i++){
                 Scanner hurt = new Scanner(System.in);
+                String hurtStringNames = names.toString().substring(1,names.toString().length()-1);
+                System.out.println(hurtStringNames + " are still alive");
                 System.out.print("Hurt: ");
                 String hurtName = hurt.nextLine();
                 Item hurt_item = itemList.get(hurtName);
                 hurt_item.hurt();
                 if (!hurt_item.aliveCheck()){
                     itemList.remove(hurt_item);
+                    names.remove(hurtName);
+                    finalNames.add(0, hurtName);
+                    int size = names.size()+1;
+                    System.out.println("Their placement was " + size);
                 }
                 Scanner heal = new Scanner(System.in);
+                String healStringNames = names.toString().substring(1,names.toString().length()-1);
+                System.out.println(healStringNames + " are still alive");
                 System.out.print("Heal: ");
                 String healName = heal.nextLine();
                 Item heal_item = itemList.get(healName);
